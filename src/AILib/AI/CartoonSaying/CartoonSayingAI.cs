993165d8faa6197f11104899b2ef7b47452262e8
Dolany <dolany@sina.cn>
2018-06-05 11:59:28 +0800

    129

---
+++ src/AILib/AI/CartoonSaying/CartoonSayingAI.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AILib.AI.CartoonSaying;
+
+namespace AILib
+{
+    [AI(Name = "CartoonSayingAI", Description = "AI for Cartoon Sayings.", IsAvailable = false)]
+    public class CartoonSayingAI : AIBase
+    {
+        public CartoonSayingAI(AIConfigDTO ConfigDTO)
+            : base(ConfigDTO)
+        {
+
+        }
+
+        public override void Work()
+        {
+            
+        }
+
+        public override void OnPrivateMsgReceived(PrivateMsgDTO MsgDTO)
+        {
+            base.OnPrivateMsgReceived(MsgDTO);
+
+
+        }
+
+        public override void OnGroupMsgReceived(GroupMsgDTO MsgDTO)
+        {
+            base.OnGroupMsgReceived(MsgDTO);
+        }
+    }
+}
---
+++ src/AILib/AI/CartoonSaying/SayingInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AILib
+{
+    public class SayingInfo
+    {
+        public string Cartoon { get; set; }
+
+        public string Charactor { get; set; }
+
+        public string Sayings { get; set; }
+    }
+}
