e50d07f67b1e1ed043c4d646cc4773f7dcc97ef5
Dolany <dolany@sina.cn>
2018-06-05 17:54:51 +0800

    160

--- src/AILib/AI/CartoonSaying/CartoonSayingAI.cs
+++ src/AILib/AI/CartoonSaying/CartoonSayingAI.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using AILib.AI.CartoonSaying;
+using System.Data.Linq;
+using Flexlive.CQP.Framework;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace AILib
 {
-    [AI(Name = "CartoonSayingAI", Description = "AI for Cartoon Sayings.", IsAvailable = false)]
+    [AI(Name = "CartoonSayingAI", Description = "AI for Cartoon Sayings.", IsAvailable = true)]
     public class CartoonSayingAI : AIBase
     {
+        private string xmlFilePath = @"./AI/CartoonSaying/Saying.xml";
+
         public CartoonSayingAI(AIConfigDTO ConfigDTO)
             : base(ConfigDTO)
         {
@@ -25,12 +30,100 @@ namespace AILib
         {
             base.OnPrivateMsgReceived(MsgDTO);
 
-
+            ProcceedMsg(MsgDTO.fromQQ, MsgDTO.msg, CQ.SendPrivateMessage);
         }
 
         public override void OnGroupMsgReceived(GroupMsgDTO MsgDTO)
         {
             base.OnGroupMsgReceived(MsgDTO);
+
+            ProcceedMsg(MsgDTO.fromGroup, MsgDTO.msg, CQ.SendGroupMessage);
+        }
+
+        private void ProcceedMsg(long fromQQ, string msg, Action<long, string> sendAction)
+        {
+            SayingInfo info = SayingInfo.Parse(msg);
+            if (info != null)
+            {
+                string smsg = SaveSaying(info) ? "语录录入成功！" : "语录录入失败！";
+                sendAction(fromQQ, smsg);
+                return;
+            }
+
+            if (msg.Trim().Equals("语录"))
+            {
+                string ranSaying = GetRanSaying();
+                if (string.IsNullOrEmpty(ranSaying))
+                {
+                    return;
+                }
+
+                sendAction(fromQQ, ranSaying);
+                return;
+            }
+        }
+
+        private bool SaveSaying(SayingInfo info)
+        {
+            try
+            {
+                XElement root = XElement.Load(xmlFilePath);
+                XElement node = new XElement("Saying", info.Sayings);
+                node.SetAttributeValue("Cartoon", info.Cartoon);
+                node.SetAttributeValue("Charactor", info.Charactor);
+                root.Add(node);
+                root.Save(xmlFilePath);
+
+                return true;
+            }
+            catch(Exception ex)
+            {
+                Common.SendMsgToDevelper(ex.Message);
+                Common.SendMsgToDevelper(ex.StackTrace);
+                return false;
+            }
+        }
+
+        private string GetRanSaying()
+        {
+            try
+            {
+                XElement root = XElement.Load(xmlFilePath);
+
+                List<SayingInfo> list = new List<SayingInfo>();
+                foreach(XElement node in root.Nodes())
+                {
+                    SayingInfo s = new SayingInfo()
+                    {
+                        Cartoon = node.Attribute("Cartoon").Value,
+                        Charactor = node.Attribute("Charactor").Value,
+                        Sayings = node.Value
+                    };
+
+                    list.Add(s);
+                }
+
+                Random random = new Random();
+                int randIdx = random.Next(list.Count);
+                
+                return GetShownSaying(list[randIdx]);
+            }
+            catch (Exception ex)
+            {
+                Common.SendMsgToDevelper(ex.Message);
+                Common.SendMsgToDevelper(ex.StackTrace);
+                return string.Empty;
+            }
+        }
+
+        private string GetShownSaying(SayingInfo s)
+        {
+            string shownSaying = $@"
+    {s.Sayings}
+    ——《{s.Cartoon}》 {s.Charactor}
+";
+
+            return shownSaying;
         }
     }
 }
--- src/AILib/AI/CartoonSaying/SayingInfo.cs
+++ src/AILib/AI/CartoonSaying/SayingInfo.cs
@@ -13,5 +13,33 @@ namespace AILib
         public string Charactor { get; set; }
 
         public string Sayings { get; set; }
+
+        public static SayingInfo Parse(string Msg)
+        {
+            if(string.IsNullOrEmpty(Msg))
+            {
+                return null;
+            }
+
+            try
+            {
+                string[] parts = Msg.Split(new char[] { ':', '：', ' ' });
+                if (parts.Length < 4 || !parts[0].Contains("语录"))
+                {
+                    return null;
+                }
+
+                return new SayingInfo()
+                {
+                    Cartoon = parts[1],
+                    Charactor = parts[2],
+                    Sayings = parts[3]
+                };
+            }
+            catch
+            {
+                return null;
+            }
+        }
     }
 }
