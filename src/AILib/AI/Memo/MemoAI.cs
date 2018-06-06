b75522583bde888f8e9fc3c4cbe9e443d6eeac51
Dolany <dolany@sina.cn>
2018-06-06 21:35:30 +0800

    Create MemoAI

---
+++ src/AILib/AI/Memo/MemoAI.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Flexlive.CQP.Framework;
+
+namespace AILib
+{
+    [AI(Name = "MemoAI", Description = "AI for Memo and Alert.", IsAvailable = true)]
+    public class MemoAI : AIBase
+    {
+        private string xmlFilePath = @"./AI/Memo/Memos.xml";
+
+        public MemoAI(AIConfigDTO ConfigDTO)
+            : base(ConfigDTO)
+        {
+
+        }
+
+        public override void Work()
+        {
+
+        }
+
+        public override void OnPrivateMsgReceived(PrivateMsgDTO MsgDTO)
+        {
+            base.OnPrivateMsgReceived(MsgDTO);
+        }
+
+        public override void OnGroupMsgReceived(GroupMsgDTO MsgDTO)
+        {
+            base.OnGroupMsgReceived(MsgDTO);
+        }
+    }
+}
