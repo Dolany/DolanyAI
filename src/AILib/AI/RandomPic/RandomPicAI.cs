a1644ac2ddab40b1c6c9340df4ea63078b0fd33a
Dolany <dolany@sina.cn>
2018-06-12 20:27:58 +0800

    145

---
+++ src/AILib/AI/RandomPic/RandomPicAI.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AILib.Entities;
+
+namespace AILib
+{
+    [AI(Name = "RandomPicAI", Description = "AI for Sending Random Pic By Keyword.", IsAvailable = false)]
+    public class RandomPicAI : AIBase
+    {
+        public RandomPicAI(AIConfigDTO ConfigDTO)
+            : base(ConfigDTO)
+        {
+
+        }
+
+        public override void Work()
+        {
+            
+        }
+
+        public override void OnGroupMsgReceived(GroupMsgDTO MsgDTO)
+        {
+            
+        }
+    }
+}
