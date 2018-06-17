07f8fb3629847f043731871b7455727d98c01f56
Dolany <dolany@sina.cn>
2018-06-17 20:53:04 +0800

    123

---
+++ src/AILib/Html/HtmlParser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace AILib.Html
+{
+    public class HtmlParser
+    {
+        private HtmlDocument document = new HtmlDocument();
+
+        public void Load(string html)
+        {
+            document.Load(html);
+        }
+    }
+}
