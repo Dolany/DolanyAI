c659c5ae5125ebba0867bd7254323f5f7eabd112
Dolany <dolany@sina.cn>
2018-06-21 16:00:21 +0800

    add something in 300report

---
+++ src/AILib/AI/Jump300Report/Jump300ReportAI.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AILib.AI.Jump300Report;
+
+namespace AILib
+{
+    [AI(
+        Name = "Jump300ReportAI",
+        Description = "AI for 300 heros report.",
+        IsAvailable = true,
+        PriorityLevel = 10
+        )]
+    public class Jump300ReportAI : AIBase
+    {
+        public Jump300ReportAI(AIConfigDTO ConfigDTO)
+            : base(ConfigDTO)
+        {
+        }
+
+        public override void Work()
+        {
+        }
+
+        [EnterCommand(
+            Command = "战绩查询",
+            SourceType = MsgType.Group,
+            AuthorityLevel = AuthorityLevel.成员,
+            Description = "查询300英雄战绩信息",
+            Syntax = " [角色名]"
+            )]
+        public void JumpReport(GroupMsgDTO MsgDTO)
+        {
+            MsgSender.Instance.PushMsg(new SendMsgDTO()
+            {
+                Aim = MsgDTO.fromGroup,
+                Type = MsgType.Group,
+                Msg = "查询中，请稍候"
+            });
+            JumpReportRequestor jr = new JumpReportRequestor(MsgDTO, ReportCallBack);
+            Task.Run(() => jr.Work());
+        }
+
+        public void ReportCallBack(GroupMsgDTO MsgDTO, string Report)
+        {
+            MsgSender.Instance.PushMsg(new SendMsgDTO()
+            {
+                Aim = MsgDTO.fromGroup,
+                Type = MsgType.Group,
+                Msg = Report
+            });
+        }
+    }
+}
--- src/AILib/AI/Jump300Report/JumpReportAnalyzer.cs
+++ src/AILib/AI/Jump300Report/JumpReportAnalyzer.cs
@@ -19,8 +19,69 @@ namespace AILib.AI.Jump300Report
 
         public string GenReport()
         {
-            // TODO
-            return string.Empty;
+            string report = string.Empty;
+            report += SummaryReport();
+            return report;
+        }
+
+        private string SummaryReport()
+        {
+            if (Lists == null || Lists.Count == 0)
+            {
+                return string.Empty;
+            }
+
+            string report = "基础信息：";
+            foreach (var r in Lists.FirstOrDefault().BaseInfo)
+            {
+                report += '\r' + r.Name + ":" + r.Value;
+            }
+
+            report += '\r' + FavoriteHeroInfo();
+
+            return report;
+        }
+
+        private string FavoriteHeroInfo()
+        {
+            if (Lists == null || Lists.Count == 0)
+            {
+                return string.Empty;
+            }
+
+            Dictionary<string, int> dic = DicHeros();
+
+            string FavoriteHero = dic.Keys.FirstOrDefault();
+            foreach (var k in dic.Keys)
+            {
+                if (dic[k] > dic[FavoriteHero])
+                {
+                    FavoriteHero = k;
+                }
+            }
+
+            return $"最常用的英雄为：{FavoriteHero}   场次：{dic[FavoriteHero]}";
+        }
+
+        private Dictionary<string, int> DicHeros()
+        {
+            Dictionary<string, int> dic = new Dictionary<string, int>();
+            foreach (var l in Lists)
+            {
+                foreach (var i in l.Matches)
+                {
+                    if (dic.Keys.Contains(i.HeroName))
+                    {
+                        dic[i.HeroName]++;
+                    }
+                    else
+                    {
+                        dic.Add(i.HeroName, 1);
+                    }
+                }
+            }
+
+            return dic;
         }
     }
 }
