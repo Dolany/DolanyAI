c66c15281152f98b72369bc1d6d5b65a17468d54
Dolany <dolany@sina.cn>
2018-07-05 18:19:04 +0800

    njl

---
+++ CQPMonitor/Tools/BlackList/BlackListTool.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CQPMonitor.Tools.BlackList;
+
+namespace CQPMonitor.Tools
+{
+    public class BlackListTool : ToolBase
+    {
+        public BlackListTool()
+        {
+            Name = "黑名单管理";
+            Decription = "管理黑名单以及屏蔽词";
+            Icon = "blacklist.ico";
+            IsAutoStart = true;
+            RelatedForm = new BlackListForm();
+        }
+
+        public override void Work()
+        {
+            base.Work();
+        }
+    }
+}
