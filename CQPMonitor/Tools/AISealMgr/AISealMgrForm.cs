416f94eefca25025ce68281a08e2671fd437c6fb
Dolany <dolany@sina.cn>
2018-07-06 09:41:43 +0800

    6

---
+++ CQPMonitor/Tools/AISealMgr/AISealMgrForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CQPMonitor.Tools.AISealMgr
+{
+    public partial class AISealMgrForm : Form
+    {
+        public AISealMgrForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
