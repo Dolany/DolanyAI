5e217dfa4f89f69acca43bb88c88288fdc7cbe95
Dolany <dolany@sina.cn>
2018-07-08 22:48:02 +0800

    kk

--- CQPMonitor/Tools/BlackList/BlackListForm.cs
+++ CQPMonitor/Tools/BlackList/BlackListForm.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using AILib;
+using AILib.Entities;
 
 namespace CQPMonitor.Tools.BlackList
 {
@@ -19,6 +21,9 @@ namespace CQPMonitor.Tools.BlackList
         )]
     public partial class BlackListForm : ToolBaseForm
     {
+        private List<BlackListEntity> BlackListList;
+        private List<DirtyWordEntity> DirtyWordList;
+
         public BlackListForm()
         {
             InitializeComponent();
@@ -26,10 +31,16 @@ namespace CQPMonitor.Tools.BlackList
 
         private void RefreshBlackListTable()
         {
+            var query = DbMgr.Query<BlackListEntity>().OrderByDescending(b => b.UpdateTime);
+            BlackListList = query.ToList();
+            blackListTable.DataSource = BlackListList;
         }
 
         private void RefreshDirtyWordsTable()
         {
+            var query = DbMgr.Query<DirtyWordEntity>();
+            DirtyWordList = query.ToList();
+            dirtyWordsTable.DataSource = DirtyWordList;
         }
 
         private void RefreshBtn_Click(object sender, EventArgs e)
@@ -40,6 +51,57 @@ namespace CQPMonitor.Tools.BlackList
         private void BlackListForm_Load(object sender, EventArgs e)
         {
             RefreshBlackListTable();
+            RefreshDirtyWordsTable();
+        }
+
+        private void NewDiretyWordBtn_Click(object sender, EventArgs e)
+        {
+            NewDirtyWordForm f = new NewDirtyWordForm();
+            if (f.ShowDialog() == DialogResult.OK)
+            {
+                RefreshDirtyWordsTable();
+            }
+        }
+
+        private void 封印ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var curRow = blackListTable.CurrentRow;
+            if (curRow == null || curRow.Index < 0)
+            {
+                return;
+            }
+
+            var entity = BlackListList[curRow.Index];
+            entity.BlackCount = 10;
+            DbMgr.Update(entity);
+
+            RefreshBlackListTable();
+        }
+
+        private void 重置ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var curRow = blackListTable.CurrentRow;
+            if (curRow == null || curRow.Index < 0)
+            {
+                return;
+            }
+
+            var entity = BlackListList[curRow.Index];
+            entity.BlackCount = 0;
+            DbMgr.Update(entity);
+
+            RefreshBlackListTable();
+        }
+
+        private void 删除ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var curRow = dirtyWordsTable.CurrentRow;
+            if (curRow == null || curRow.Index < 0)
+            {
+                return;
+            }
+
+            DbMgr.Delete<DirtyWordEntity>(DirtyWordList[curRow.Index].Id);
             RefreshDirtyWordsTable();
         }
     }
---
+++ CQPMonitor/Tools/BlackList/NewDirtyWordForm.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AILib;
+using AILib.Entities;
+
+namespace CQPMonitor.Tools.BlackList
+{
+    public partial class NewDirtyWordForm : Form
+    {
+        public NewDirtyWordForm()
+        {
+            InitializeComponent();
+        }
+
+        private void cancelBtn_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+        }
+
+        private void SaveBtn_Click(object sender, EventArgs e)
+        {
+            string word = wordTxt.Text;
+            if (string.IsNullOrEmpty(word))
+            {
+                MessageBox.Show("屏蔽词不能为空！");
+                wordTxt.Focus();
+                return;
+            }
+
+            var query = DbMgr.Query<DirtyWordEntity>(d => d.Content == word);
+            if (!query.IsNullOrEmpty())
+            {
+                MessageBox.Show("屏蔽词已存在！");
+                wordTxt.Focus();
+                return;
+            }
+
+            DbMgr.Insert(new DirtyWordEntity()
+            {
+                Id = Guid.NewGuid().ToString(),
+                Content = word
+            });
+            this.DialogResult = DialogResult.OK;
+        }
+    }
+}
