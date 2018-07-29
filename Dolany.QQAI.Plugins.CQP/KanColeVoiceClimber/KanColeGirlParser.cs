d243e2832773b0508fdf4ad2810bbd9abce8e49e
Dolany <dolany@sina.cn>
2018-07-29 19:24:37 +0800

    123456789

--- Dolany.QQAI.Plugins.CQP/KanColeVoiceClimber/KanColeGirlParser.cs
+++ Dolany.QQAI.Plugins.CQP/KanColeVoiceClimber/KanColeGirlParser.cs
@@ -15,10 +15,7 @@ namespace KanColeVoiceClimber
         private string GirlName;
 
         private string[] timeStrs = {
-            "0000", "0100", "0200", "0300", "0400", "0500",
-            "0600", "0700", "0800", "0900", "1000", "1100",
-            "1200", "1300", "1400", "1500", "1600", "1700",
-            "1800", "1900", "2000", "2100", "2200", "2300"
+            "0000"
         };
 
         protected override void Parse()
@@ -34,24 +31,33 @@ namespace KanColeVoiceClimber
                 && p.ChildAttributes("class").First().Value == "wikitable"
                 );
 
-            var rowNodes = SearchNodes(
-                parentNode[1],
+            foreach (var node in parentNode)
+            {
+                var rowNodes = SearchNodes(
+                node,
                 p => p.Name == "a"
                 && !p.ChildAttributes("data-filesrc").IsNullOrEmpty()
                 && p.ChildAttributes("data-filesrc").First().Value.Contains(".mp3")
                 );
-            var outerNodes = rowNodes.Select(p => p.ParentNode
-                                                   .ParentNode
-                                                   .ParentNode
-                                                   .ParentNode
-                                                   .ParentNode
-                                                   .ParentNode
-                                                   .ParentNode
-                                                   );
-            var descList = GetDescList(outerNodes);
-            for (int i = 0; i < rowNodes.Count; i++)
-            {
-                ParseInfo(rowNodes[i], descList[i]);
+                if (rowNodes.Count == 0)
+                {
+                    continue;
+                }
+
+                var outerNodes = rowNodes.Select(p => p.ParentNode
+                                                       .ParentNode
+                                                       .ParentNode
+                                                       .ParentNode
+                                                       .ParentNode
+                                                       .ParentNode
+                                                       .ParentNode
+                                                       );
+
+                var descList = GetDescList(outerNodes);
+                for (int i = 0; i < rowNodes.Count; i++)
+                {
+                    ParseInfo(rowNodes[i], descList[i]);
+                }
             }
         }
 
@@ -60,7 +66,7 @@ namespace KanColeVoiceClimber
             List<string> list = new List<string>();
             foreach (var node in outerNodes)
             {
-                var tdNode = node.ChildNodes[1];
+                var tdNode = node.ChildNodes.Where(p => p.InnerText.Contains("00")).First();
                 list.Add(tdNode.InnerText.Replace("\n", ""));
             }
 
