0053f14e7d3ee6f3a6d67e070b34bbdd1b6ab3eb
Dolany <dolany@sina.cn>
2018-07-29 16:33:37 +0800

    complete kancole girl

--- Dolany.QQAI.Plugins.CQP/KanColeVoiceClimber/Form1.cs
+++ Dolany.QQAI.Plugins.CQP/KanColeVoiceClimber/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Dolany.Ice.Ai.DolanyAI;
+using Dolany.Ice.Ai.DolanyAI.Db;
 
 namespace KanColeVoiceClimber
 {
@@ -28,7 +29,7 @@ namespace KanColeVoiceClimber
             HttpRequester requester = new HttpRequester();
 
             AppendTxt("请求列表页面中...");
-            string aimStr = $"https://zh.moegirl.org/%E8%88%B0%E9%98%9FCollection/%E5%9B%BE%E9%89%B4/%E8%88%B0%E5%A8%98";
+            string aimStr = $"https://zh.moegirl.org/zh-hans/%E8%88%B0%E9%98%9FCollection/%E5%9B%BE%E9%89%B4/%E8%88%B0%E5%A8%98";
             string HtmlStr = requester.Request(aimStr);
 
             AppendTxt("解析列表页面中...");
@@ -41,19 +42,58 @@ namespace KanColeVoiceClimber
             {
                 ParseAGirl(name);
             }
+
+            AppendTxt("任务结束！");
         }
 
         private void AppendTxt(string text)
         {
             this.Invoke(new Action(() =>
             {
-                ShowTxt.AppendText(text + '\r');
+                ShowTxt.AppendText(text + "\r\n");
             }));
         }
 
         private void ParseAGirl(string name)
         {
             AppendTxt($"正在解析{name}...");
+            HttpRequester requester = new HttpRequester();
+
+            AppendTxt("请求列表页面中...");
+            string aimStr = $"https://zh.moegirl.org/{Utility.UrlCharConvert(name)}";
+            string HtmlStr = requester.Request(aimStr);
+
+            AppendTxt("解析列表页面中...");
+
+            KanColeGirlParser parser = new KanColeGirlParser();
+            parser.Load(HtmlStr);
+
+            var list = parser.kanColeGirlVoices;
+            AppendTxt($"共找到{list.Count}个语音信息，正在同步到数据库...");
+            foreach (var voice in list)
+            {
+                SynToDb(voice);
+            }
+            AppendTxt("同步完成！");
+        }
+
+        private void SynToDb(KanColeGirlVoice voice)
+        {
+            using (AIDatabase db = new AIDatabase())
+            {
+                var query = db.KanColeGirlVoice.Where(k => k.Tag == voice.Tag && k.Name == voice.Name);
+                if (query.IsNullOrEmpty())
+                {
+                    db.KanColeGirlVoice.Add(voice);
+                }
+                else
+                {
+                    var kan = query.First();
+                    kan.Content = voice.Content;
+                    kan.VoiceUrl = voice.VoiceUrl;
+                }
+                db.SaveChanges();
+            }
         }
     }
 }
--- Dolany.QQAI.Plugins.CQP/KanColeVoiceClimber/KanColeGirlParser.cs
+++ Dolany.QQAI.Plugins.CQP/KanColeVoiceClimber/KanColeGirlParser.cs
@@ -5,16 +5,100 @@ using System.Text;
 using System.Threading.Tasks;
 using Dolany.Ice.Ai.DolanyAI;
 using HtmlAgilityPack;
+using Dolany.Ice.Ai.DolanyAI.Db;
 
 namespace KanColeVoiceClimber
 {
     public class KanColeGirlParser : HtmlParser
     {
+        public List<KanColeGirlVoice> kanColeGirlVoices = new List<KanColeGirlVoice>();
+        private string GirlName;
+
+        private string[] timeStrs = {
+            "0000", "0100", "0200", "0300", "0400", "0500",
+            "0600", "0700", "0800", "0900", "1000", "1100",
+            "1200", "1300", "1400", "1500", "1600", "1700",
+            "1800", "1900", "2000", "2100", "2200", "2300"
+        };
+
         protected override void Parse()
         {
             base.Parse();
 
             var root = document.DocumentNode;
+            ParseTitle(root);
+            var parentNode = SearchNodes(
+                root,
+                p => p.Name == "table"
+                && !p.ChildAttributes("class").IsNullOrEmpty()
+                && p.ChildAttributes("class").First().Value == "wikitable"
+                );
+
+            var rowNodes = SearchNodes(
+                parentNode[1],
+                p => p.Name == "a"
+                && !p.ChildAttributes("data-filesrc").IsNullOrEmpty()
+                && p.ChildAttributes("data-filesrc").First().Value.Contains(".mp3")
+                );
+            var outerNodes = rowNodes.Select(p => p.ParentNode
+                                                   .ParentNode
+                                                   .ParentNode
+                                                   .ParentNode
+                                                   .ParentNode
+                                                   .ParentNode
+                                                   .ParentNode
+                                                   );
+            var descList = GetDescList(outerNodes);
+            for (int i = 0; i < rowNodes.Count; i++)
+            {
+                ParseInfo(rowNodes[i], descList[i]);
+            }
+        }
+
+        private List<string> GetDescList(IEnumerable<HtmlNode> outerNodes)
+        {
+            List<string> list = new List<string>();
+            foreach (var node in outerNodes)
+            {
+                var tdNode = node.ChildNodes[1];
+                list.Add(tdNode.InnerText.Replace("\n", ""));
+            }
+
+            return list;
+        }
+
+        private void ParseTitle(HtmlNode root)
+        {
+            var title = SearchNodes(root, p => p.Name == "title").First();
+            var strs = title.InnerText.Split(new char[] { ' ' });
+            GirlName = strs[0];
+        }
+
+        private void ParseInfo(HtmlNode voiceNode, string descStr)
+        {
+            string[] strs = descStr.Split(new char[] { '：' }, StringSplitOptions.RemoveEmptyEntries);
+            if (strs.IsNullOrEmpty()
+                || strs.Length != 3
+                )
+            {
+                return;
+            }
+
+            if (!timeStrs.Contains(strs[0].Trim()))
+            {
+                return;
+            }
+
+            KanColeGirlVoice voice = new KanColeGirlVoice
+            {
+                Id = Guid.NewGuid().ToString(),
+                Content = strs[2].Trim(),
+                VoiceUrl = voiceNode.ChildAttributes("data-filesrc").First().Value,
+                Name = GirlName,
+                Tag = strs[0].Trim()
+            };
+
+            kanColeGirlVoices.Add(voice);
         }
     }
 }
