7841de0c1d8092c8cf3e566214d3082c9fc11248
Dolany <dolany@sina.cn>
2018-08-07 17:04:39 +0800

    add kancolevoice

---
+++ Dolany.QQAI.Plugins.CQP/Dolany.Ice.Ai/DolanyAI/AI/Voice/KancoleVoiceAI.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dolany.Ice.Ai.DolanyAI
+{
+    [AI(
+        Name = "KancoleVoiceAI",
+        Description = "AI for response random kancole girl voice.",
+        IsAvailable = true,
+        PriorityLevel = 10
+        )]
+    public class KancoleVoiceAI : AIBase
+    {
+        public KancoleVoiceAI()
+            : base()
+        {
+            RuntimeLogger.Log("KancoleVoiceAI started.");
+        }
+
+        public override void Work()
+        {
+        }
+    }
+}
