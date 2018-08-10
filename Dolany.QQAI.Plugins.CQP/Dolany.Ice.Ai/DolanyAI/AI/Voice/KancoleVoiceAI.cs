98ebc03ed0eec648f051b44906e88509dba0031a
Dolany <dolany@sina.cn>
2018-08-10 10:02:23 +0800

    complete kancolevoice

--- Dolany.QQAI.Plugins.CQP/Dolany.Ice.Ai/DolanyAI/AI/Voice/KancoleVoiceAI.cs
+++ Dolany.QQAI.Plugins.CQP/Dolany.Ice.Ai/DolanyAI/AI/Voice/KancoleVoiceAI.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dolany.Ice.Ai.DolanyAI.Db;
+using Dolany.Ice.Ai.MahuaApis;
 
 namespace Dolany.Ice.Ai.DolanyAI
 {
@@ -22,6 +24,56 @@ namespace Dolany.Ice.Ai.DolanyAI
 
         public override void Work()
         {
+        }
+
+        [GroupEnterCommand(
+            Command = "舰娘语音",
+            AuthorityLevel = AuthorityLevel.成员,
+            Description = "获取随机舰娘语音",
+            Syntax = "[舰娘名称]",
+            Tag = "语音功能",
+            SyntaxChecker = "NotEmpty"
+            )]
+        public void KancoleVoice(GroupMsgDTO MsgDTO, object[] param)
+        {
+            string girlName = param[0] as string;
+
+            var voice = GetRandVoiceInfo(girlName);
+            if (voice == null)
+            {
+                return;
+            }
+
+            MsgSender.Instance.PushMsg(new SendMsgDTO
+            {
+                Aim = MsgDTO.FromGroup,
+                Type = MsgType.Group,
+                Msg = CodeApi.Code_Voice(voice.VoiceUrl)
+            });
+            MsgSender.Instance.PushMsg(new SendMsgDTO
+            {
+                Aim = MsgDTO.FromGroup,
+                Type = MsgType.Group,
+                Msg = voice.Content
+            });
+        }
+
+        private KanColeGirlVoice GetRandVoiceInfo(string name)
+        {
+            using (AIDatabase db = new AIDatabase())
+            {
+                var query = db.KanColeGirlVoice.Where(p => p.Name.Contains(name)).OrderBy(p => p.Id);
+                if (query.IsNullOrEmpty())
+                {
+                    return null;
+                }
+
+                int count = query.Count();
+                Random random = new Random();
+                int idx = random.Next(count);
+
+                return query.Skip(idx).First().Clone();
+            }
         }
     }
 }
