0c41baf203f7de239cdca06b0022333d165c034a
Dolany <dolany@sina.cn>
2018-09-10 00:02:11 +0800

    23

--- Dolany.QQAI.Plugins.CQP/Dolany.IWS2000.Ai/MahuaModule.cs
+++ Dolany.QQAI.Plugins.CQP/Dolany.IWS2000.Ai/MahuaModule.cs
@@ -54,7 +54,7 @@ namespace Dolany.IWS2000.Ai
                 // 将需要监听的事件注册，若缺少此注册，则不会调用相关的实现类
 
                 builder.RegisterType<GroupMsgReceived>()
-                    .As<IPrivateMessageFromFriendReceivedMahuaEvent>();
+                    .As<IGroupMessageReceivedMahuaEvent>();
             }
         }
 
