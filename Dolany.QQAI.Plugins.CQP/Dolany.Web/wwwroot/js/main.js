0276f01b602575bc7f30b693c12ad8b5be3ec385
Dolany <dolany@sina.cn>
2019-01-25 22:53:19 +0800

    63

---
+++ Dolany.QQAI.Plugins.CQP/Dolany.Web/wwwroot/js/main.js
@@ -0,0 +1,11 @@
+﻿var main = new Vue({
+    el: '#nav',
+    data: {
+        activeIndex: '1'
+    },
+    methods: {
+        handleSelect(key, keyPath) {
+            console.log(key, keyPath);
+        }
+    }
+})
