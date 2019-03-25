ab6189fa9483728be0b6b1f41516495f2d37f343
Dolany <dolany@sina.cn>
2019-03-25 13:05:42 +0800

    23

--- Dolany.QQAI.Plugins.CQP/Dolany.Game.Advanture/AdvGameEngine.cs
+++ Dolany.QQAI.Plugins.CQP/Dolany.Game.Advanture/AdvGameEngine.cs
@@ -50,7 +50,7 @@ namespace Dolany.Game.Advanture
                 {
                     ProcessTurn();
                     AlterTurn();
-                    Thread.Sleep(1000);
+                    Thread.Sleep(2000);
                 }
 
                 Settlement();
@@ -79,69 +79,67 @@ namespace Dolany.Game.Advanture
 
         private void ProcessCave(ICave cave)
         {
-            var msg = $"你遇到了 {cave.Description} ！";
-            CommonUtil.MsgSendBack(GroupNum, CurPlayer.QQNum, msg);
-            Thread.Sleep(1000);
+            var msg = $"你遇到了 {cave.Description} ！\r";
+            Thread.Sleep(2000);
 
             switch (cave.Type)
             {
                 case CaveType.宝箱:
-                    ProcessTreasureCave(cave as TreasureCave);
+                    msg += ProcessTreasureCave(cave as TreasureCave);
                     break;
                 case CaveType.陷阱:
-                    ProcessTrapCave(cave as TrapCave);
+                    msg += ProcessTrapCave(cave as TrapCave);
                     break;
                 case CaveType.怪兽:
-                    ProcessMonsterCave(cave as MonsterCave);
+                    msg += ProcessMonsterCave(cave as MonsterCave);
                     break;
             }
 
+            CommonUtil.MsgSendBack(GroupNum, CurPlayer.QQNum, msg);
             cave.Visible = true;
         }
 
-        private void ProcessMonsterCave(MonsterCave cave)
+        private string ProcessMonsterCave(MonsterCave cave)
         {
+            var msg = "";
             var atk = CurPlayer.GetAtk();
             cave.HP = Math.Max(cave.HP - atk, 0);
-            CommonUtil.MsgSendBack(GroupNum, CurPlayer.QQNum, 
-                $"你打出了 {atk} 点伤害，{cave.Name}剩余{cave.HP}{Emoji.心}");
-            Thread.Sleep(1000);
+            msg += $"你打出了 {atk} 点伤害，{cave.Name}剩余{cave.HP}{Emoji.心}\r";
             if (cave.HP > 0)
             {
                 CurPlayer.HP = Math.Max(CurPlayer.HP - cave.Atk, 0);
-                CommonUtil.MsgSendBack(GroupNum, CurPlayer.QQNum, 
-                    $"{cave.Name}对你造成了{cave.Atk}点伤害，你剩余{CurPlayer.HP}{Emoji.心}");
+                msg += $"{cave.Name}对你造成了{cave.Atk}点伤害，你剩余{CurPlayer.HP}{Emoji.心}";
             }
             else
             {
                 OtherPlayer.HP = Math.Max(OtherPlayer.HP - cave.Atk, 0);
-                CommonUtil.MsgSendBack(GroupNum, OtherPlayer.QQNum, 
-                    $"你受到了{cave.Atk}点伤害，你剩余{OtherPlayer.HP}{Emoji.心}");
+                msg += $"{CodeApi.Code_At(OtherPlayer.QQNum)} 你受到了{cave.Atk}点伤害，你剩余{OtherPlayer.HP}{Emoji.心}";
             }
+
+            return msg;
         }
 
-        private void ProcessTreasureCave(TreasureCave cave)
+        private string ProcessTreasureCave(TreasureCave cave)
         {
+            var msg = "";
             var atk = CurPlayer.GetAtk();
             cave.HP = Math.Max(cave.HP - atk, 0);
-            CommonUtil.MsgSendBack(GroupNum, CurPlayer.QQNum, 
-                $"你打出了 {atk} 点伤害，{cave.Name}剩余{cave.HP}{Emoji.心}");
+            msg += $"你打出了 {atk} 点伤害，{cave.Name}剩余{cave.HP}{Emoji.心}";
             Thread.Sleep(1000);
             if (cave.HP > 0)
             {
-                return;
+                return msg;
             }
 
             Bonus += cave.Golds;
-            CommonUtil.MsgSendBack(GroupNum, CurPlayer.QQNum,
-                $"你击碎了宝箱！当前赏金为 {Bonus}{Emoji.钱}！赢得对决的人将获得全部赏金！");
+            msg += $"\r你击碎了宝箱！当前赏金为 {Bonus}{Emoji.钱}！赢得对决的人将获得全部赏金！";
+            return msg;
         }
 
-        private void ProcessTrapCave(TrapCave cave)
+        private string ProcessTrapCave(TrapCave cave)
         {
             CurPlayer.HP = Math.Max(CurPlayer.HP -= cave.Atk, 0);
-            CommonUtil.MsgSendBack(GroupNum, CurPlayer.QQNum, 
-                $"你受到了 {cave.Atk}点伤害，剩余生命值 {CurPlayer.HP}{Emoji.心}");
+            return $"你受到了 {cave.Atk}点伤害，剩余生命值 {CurPlayer.HP}{Emoji.心}";
         }
 
         private void RefreshCave(int idx)
