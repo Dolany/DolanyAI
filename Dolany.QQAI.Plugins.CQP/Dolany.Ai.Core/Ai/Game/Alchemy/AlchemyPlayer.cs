1b2e8bcfebac1e2a52e9bfd5eb1a660677446b90
liuchenxi <liuchenxi@@mallcoo.cn>
2019-04-19 13:38:22 +0800

    2

--- Dolany.QQAI.Plugins.CQP/Dolany.Ai.Core/Ai/Game/Alchemy/AlchemyPlayer.cs
+++ Dolany.QQAI.Plugins.CQP/Dolany.Ai.Core/Ai/Game/Alchemy/AlchemyPlayer.cs
@@ -1,8 +1,18 @@
-﻿using Dolany.Database;
+﻿using System.Collections.Generic;
+using Dolany.Database;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace Dolany.Ai.Core.Ai.Game.Alchemy
 {
+    [BsonIgnoreExtraElements]
     public class AlchemyPlayer : DbBaseEntity
     {
+        public long QQNum { get; set; }
+
+        public string Name { get; set; }
+
+        public int Level { get; set; } = 1;
+
+        public List<string> Titles { get; set; } = new List<string>();
     }
 }
