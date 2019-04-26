d6285232ea03c7084f5c041d9b83c63e2bfc54ca
liuchenxi <liuchenxi@mallcoo.cn>
2019-04-26 18:32:48 +0800

    re

---
+++ Dolany.QQAI.Plugins.CQP/Dolany.Ai.Core/Ai/Game/Cooking/CookRelationship.cs
@@ -0,0 +1,11 @@
+﻿using Dolany.Database;
+
+namespace Dolany.Ai.Core.Ai.Game.Cooking
+{
+    public class CookRelationship : DbBaseEntity
+    {
+        public long[] QQPair { get; set; }
+
+        public int Relationship { get; set; }
+    }
+}
