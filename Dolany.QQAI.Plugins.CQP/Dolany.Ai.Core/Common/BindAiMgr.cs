de217eb7ac0c448c7d31d4b6be6edd961ef2d235
liuchenxi <liuchenxi@mallcoo.cn>
2019-05-13 10:30:32 +0800

    asd

---
+++ Dolany.QQAI.Plugins.CQP/Dolany.Ai.Core/Common/BindAiMgr.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Dolany.Ai.Common;
+
+namespace Dolany.Ai.Core.Common
+{
+    public class BindAiMgr
+    {
+        public static BindAiMgr Instance { get; } = new BindAiMgr();
+
+        public Dictionary<string, BindAiModel> AiDic;
+
+        private BindAiMgr()
+        {
+            AiDic = CommonUtil.ReadJsonData<Dictionary<string, BindAiModel>>("BindAiData");
+        }
+    }
+
+    public class BindAiModel
+    {
+        public long SelfNum { get; set; }
+
+        public string CommandQueue { get; set; }
+    }
+}
