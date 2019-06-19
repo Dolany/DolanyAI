f4965e6733807fec7e6f6543b5e541371e65ac56
Dolany <dolany@sina.cn>
2019-06-19 19:01:23 +0800

    upgrading code

--- Dolany.QQAI.Plugins.CQP/Dolany.Temp/Program.cs
+++ Dolany.QQAI.Plugins.CQP/Dolany.Temp/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using Dolany.Ai.Core.OnlineStore;
+using Dolany.Database;
+using Dolany.Database.Ai;
 
 namespace Dolany.Temp
 {
@@ -6,18 +9,42 @@ namespace Dolany.Temp
     {
         static void Main(string[] args)
         {
-            //var record = new DailyLimitRecord()
-            //{
-            //    QQNum = 123, Commands = new Dictionary<string, DailyLimitCommand>() {{"a", new DailyLimitCommand() {Times = 0, ExpiryTime = CommonUtil.UntilTommorow().AddDays(-1)}}}
-            //};
-            //var rr = record.Check("a", 1);
-            //record.Cache("a");
+            var oldRecords = MongoService<DriftItemRecord>.Get();
+            Console.WriteLine("Total Counts: " + oldRecords.Count);
+            var c = 0;
+            foreach (var oldRecord in oldRecords)
+            {
+                var newRecord = ItemCollectionRecord.Get(oldRecord.QQNum);
+                foreach (var countRecord in oldRecord.ItemCount)
+                {
+                    var honorName = HonorHelper.Instance.FindHonorName(countRecord.Name);
+                    var honor = HonorHelper.Instance.FindHonor(honorName);
 
-            //rr = record.Check("a", 1);
-            //record.Cache("a");
+                    if (!newRecord.HonorCollections.ContainsKey(honorName))
+                    {
+                        newRecord.HonorCollections.Add(honorName, new HonorItemCollection()
+                        {
+                            Name = honorName,
+                            Type = honor is LimitHonorModel ? HonorType.Limit : HonorType.Normal
+                        });
+                    }
 
-            //rr = record.Check("a", 1);
-            //record.Cache("a");
+                    var collection = newRecord.HonorCollections[honorName];
+                    if (!collection.Items.ContainsKey(countRecord.Name))
+                    {
+                        collection.Items.Add(countRecord.Name, 0);
+                    }
+
+                    collection.Items[countRecord.Name] = countRecord.Count;
+                }
+
+                c++;
+                if (c % 100 == 0)
+                {
+                    Console.WriteLine($"{c} completed.");
+                }
+                newRecord.Update();
+            }
 
             Console.WriteLine("Completed");
             Console.ReadKey();
