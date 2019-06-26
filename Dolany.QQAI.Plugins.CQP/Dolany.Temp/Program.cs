f5c289a48f26a6d97bb3fb6f75f67727cf4e8a7e
Dolany <dolany@sina.cn>
2019-06-26 23:09:39 +0800

    测试

--- Dolany.QQAI.Plugins.CQP/Dolany.Temp/Program.cs
+++ Dolany.QQAI.Plugins.CQP/Dolany.Temp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Dolany.Ai.Common;
 
 namespace Dolany.Temp
 {
@@ -6,7 +7,18 @@ namespace Dolany.Temp
     {
         static void Main(string[] args)
         {
-            
+            var participates = new[]
+            {
+                "子衣不待", "Visca120", "沧月风语", "斯普林菲尔德太太", "Ilovepeaches",
+                "舞舞逸", "鸢一啦", "月牙儿", "苏花半笔墨留白", "猹猹猹猹猹", "苏琳",
+                "一只呆雪", "我是叮当啊好的", "凌天澄空", "萌希灵", "取名好难不想了 "
+            };
+            var randArray = CommonUtil.RandSort(participates);
+            for (var i = 0; i < randArray.Length; i += 4)
+            {
+                Console.Write(randArray[i] + ',' + randArray[i + 1] + ',' + randArray[i + 2] + ',' + randArray[i + 3]);
+                Console.WriteLine();
+            }
 
             Console.WriteLine("Completed");
             Console.ReadKey();
