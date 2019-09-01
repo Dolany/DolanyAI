9239192a6cd11c11ce13e2300910b34ca8e0eaa3
Dolany <dolany@sina.cn>
2019-09-02 00:24:54 +0800

    3659

--- Dolany.QQAI.Plugins.CQP/Dolany.Temp/Program.cs
+++ Dolany.QQAI.Plugins.CQP/Dolany.Temp/Program.cs
@@ -10,12 +10,15 @@ namespace Dolany.Temp
     {
         static void Main(string[] args)
         {
-            var itemColl = ItemCollectionRecord.Get(1304399144);
+            var itemColl = ItemCollectionRecord.Get(3325883147);
             var items = itemColl.HonorCollections.Values.SelectMany(h => h.Items.Keys);
             Console.WriteLine(items.Count());
 
-            var allItems = HonorHelper.Instance.HonorList.SelectMany(h => h.Items);
+            var allItems = HonorHelper.Instance.HonorList.SelectMany(h => h.Items).Select(p => p.Name);
             Console.WriteLine(allItems.Count());
+
+            var des = allItems.Except(items);
+            Console.WriteLine(string.Join(",", des));
 
             Console.WriteLine("Completed");
             Console.ReadKey();
