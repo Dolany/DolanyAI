78117188b3e1fd9e4ded75004c8e4850c222c010
Dolany <dolany@sina.cn>
2019-10-30 18:45:46 +0800

    cooking

---
+++ Dolany.QQAI.Plugins.CQP/Dolany.Ai.Core/Ai/Game/Pet/Cooking/CookingDietMgr.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Dolany.Ai.Common;
+
+namespace Dolany.Ai.Core.Ai.Game.Pet.Cooking
+{
+    public class CookingDietMgr
+    {
+        public static CookingDietMgr Instance { get; } = new CookingDietMgr();
+
+        private readonly List<CookingDietModel> DietList;
+
+        private CookingDietMgr()
+        {
+            DietList = CommonUtil.ReadJsonData_NamedList<CookingDietModel>("CookingDietData");
+        }
+    }
+
+    public class CookingDietModel : INamedJsonModel
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ Dolany.QQAI.Plugins.CQP/Dolany.Ai.Core/Ai/Game/Pet/Cooking/CookingRecord.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using Dolany.Ai.Common;
+using Dolany.Database;
+
+namespace Dolany.Ai.Core.Ai.Game.Pet.Cooking
+{
+    public class CookingRecord : DbBaseEntity
+    {
+        public long QQNum { get; set; }
+
+        public Dictionary<string, int> CookedDietDic { get; set; }
+
+        public Dictionary<string, int> FlavoringDic { get; set; }
+
+        public static CookingRecord Get(long QQNum)
+        {
+            var record = MongoService<CookingRecord>.GetOnly(p => p.QQNum == QQNum);
+            if (record != null)
+            {
+                return record;
+            }
+
+            record = new CookingRecord(){QQNum = QQNum};
+            MongoService<CookingRecord>.Insert(record);
+
+            return record;
+        }
+
+        public void Update()
+        {
+            CookedDietDic.Remove(p => p == 0);
+            FlavoringDic.Remove(p => p == 0);
+
+            MongoService<CookingRecord>.Update(this);
+        }
+    }
+}
