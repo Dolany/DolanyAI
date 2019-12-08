618e24988d189dc58d88ff92944d169cc7989f96
Dolany <dolany@sina.cn>
2019-12-08 21:57:15 +0800

    63

--- Dolany.QQAI.Plugins.CQP/Dolany.Ai.Core/Ai/SingleCommand/Dice/DiceAI.cs
+++ Dolany.QQAI.Plugins.CQP/Dolany.Ai.Core/Ai/SingleCommand/Dice/DiceAI.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using Dolany.Ai.Common;
 using Dolany.Ai.Common.Models;
 using Dolany.Ai.Core.Base;
@@ -40,6 +41,23 @@ namespace Dolany.Ai.Core.Ai.SingleCommand.Dice
                 return false;
             }
 
+            if (!ParseFormat1(MsgDTO) && !ParseFormat2(MsgDTO))
+            {
+                return false;
+            }
+
+            AIAnalyzer.AddCommandCount(new CommandAnalyzeDTO()
+            {
+                Ai = AIName,
+                Command = "DiceOverride",
+                GroupNum = MsgDTO.FromGroup,
+                BindAi = MsgDTO.BindAi
+            });
+            return true;
+        }
+
+        private bool ParseFormat1(MsgInformationEx MsgDTO)
+        {
             var format = MsgDTO.Command;
 
             var model = ParseDice(format);
@@ -50,13 +68,50 @@ namespace Dolany.Ai.Core.Ai.SingleCommand.Dice
 
             var result = ConsoleDice(model);
             SendResult(MsgDTO, result);
-            AIAnalyzer.AddCommandCount(new CommandAnalyzeDTO()
+            return true;
+        }
+
+        private bool ParseFormat2(MsgInformationEx MsgDTO)
+        {
+            if (!MsgDTO.FullMsg.Contains(".r"))
             {
-                Ai = AIName,
-                Command = "DiceOverride",
-                GroupNum = MsgDTO.FromGroup,
-                BindAi = MsgDTO.BindAi
+                return false;
+            }
+
+            if (MsgDTO.FullMsg.Trim() == ".r")
+            {
+                var randInt = Rander.RandInt(6) + 1;
+                MsgSender.PushMsg(MsgDTO, $"你掷出了 {randInt}点！", true);
+                return true;
+            }
+
+            var afterPart = MsgDTO.FullMsg.Replace(".r", string.Empty).Trim();
+            if (int.TryParse(afterPart, out var extra))
+            {
+                var randInt = Rander.RandInt(6) + 1;
+                MsgSender.PushMsg(MsgDTO, $"你掷出了 {randInt}+{extra}={randInt + extra}点！", true);
+                return true;
+            }
+
+            if (!afterPart.StartsWith("*"))
+            {
+                return false;
+            }
+
+            var multiPart = afterPart.Replace("*", string.Empty).Trim();
+            if (!int.TryParse(multiPart, out var multiNum) || multiNum <= 0 || multiNum > DiceCountMaxLimit)
+            {
+                return false;
+            }
+
+            var randInts = Enumerable.Range(0, multiNum).Select(p =>
+            {
+                Thread.Sleep(10);
+                return Rander.RandInt(6) + 1;
             });
+            var intMsg = string.Join("+", randInts);
+            var msg = $"你掷出了 {intMsg}={randInts.Sum()}点！";
+            MsgSender.PushMsg(MsgDTO, msg, true);
             return true;
         }
 
