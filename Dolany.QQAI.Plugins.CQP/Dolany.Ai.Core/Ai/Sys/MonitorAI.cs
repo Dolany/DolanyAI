55e033d172d9aa2f32eb71f05b1d44b38a3aba3c
Dolany <dolany@sina.cn>
2019-12-17 14:27:43 +0800

    自助充值

--- Dolany.QQAI.Plugins.CQP/Dolany.Ai.Core/Ai/Sys/MonitorAI.cs
+++ Dolany.QQAI.Plugins.CQP/Dolany.Ai.Core/Ai/Sys/MonitorAI.cs
@@ -4,6 +4,7 @@ using Dolany.Ai.Common.Models;
 using Dolany.Ai.Core.Base;
 using Dolany.Ai.Core.Cache;
 using Dolany.Ai.Core.Common;
+using Dolany.Ai.Core.OnlineStore;
 
 namespace Dolany.Ai.Core.Ai.Sys
 {
@@ -92,6 +93,38 @@ namespace Dolany.Ai.Core.Ai.Sys
             groupInfo.Update();
 
             MsgSender.PushMsg(MsgDTO, "开机成功！");
+            return true;
+        }
+
+        [EnterCommand(ID = "MonitorAI_SelfExcharge",
+            Command = "自助充值",
+            Description = "使用钻石自助为本群充值指定天数的机器人使用时间(1天=10钻石)",
+            Syntax = "[天数]",
+            Tag = "系统命令",
+            SyntaxChecker = "Long",
+            AuthorityLevel = AuthorityLevel.成员,
+            IsPrivateAvailable = false)]
+        public bool SelfExcharge(MsgInformationEx MsgDTO, object[] param)
+        {
+            var days = (int) (long) param[0];
+            if (days <= 0)
+            {
+                MsgSender.PushMsg(MsgDTO, "天数错误，请重新输入命令！");
+                return false;
+            }
+
+            var osPerson = OSPerson.GetPerson(MsgDTO.FromQQ);
+            var diamondNeed = days * 10;
+            if (osPerson.Diamonds < diamondNeed)
+            {
+                MsgSender.PushMsg(MsgDTO, "你的钻石余额不足，请添加能天使(2731544408)为好友后，使用【转账】功能转任意金额后将会获得金额*100的钻石，可以【我的状态】命令查看余额！");
+                return false;
+            }
+
+            AIMgr.Instance.AIInstance<DeveloperOnlyAI>().ChargeTime(MsgDTO, new object[] {MsgDTO.FromGroup, days});
+            osPerson.Diamonds -= diamondNeed;
+            osPerson.Update();
+
             return true;
         }
 
