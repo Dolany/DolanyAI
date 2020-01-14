2f2e8cb023b293a748d44e143bdd1b8c8d2fc41c
Dolany <dolany@sina.cn>
2020-01-14 13:47:34 +0800

    update nuget

--- Dolany.QQAI.Plugins.CQP/Dolany.Database/Sqlite/SqliteContext.cs
+++ Dolany.QQAI.Plugins.CQP/Dolany.Database/Sqlite/SqliteContext.cs
@@ -1,13 +1,18 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System;
+using Microsoft.EntityFrameworkCore;
 
 namespace Dolany.Database.Sqlite
 {
-    public class SqliteContext : BaseCacheContent
+    public class SqliteContext : BaseCacheContent, IDisposable
     {
         public DbSet<SqliteCacheModel> SqliteCacheModel { get; set; }
         public DbSet<SqliteFixedSet> SqliteFixedSet { get; set; }
 
         public SqliteContext(string source) : base(source)
+        {
+        }
+
+        public void Dispose()
         {
         }
     }
