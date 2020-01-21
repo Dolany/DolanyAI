2c1b8c5d87335474cbb2533104615f29ffd376a2
Dolany <dolany@sina.cn>
2020-01-21 18:23:10 +0800

    SERecord

--- Dolany.QQAI.Plugins.CQP/Dolany.WorldLine.Standard/Ai/Game/SwordExplore/SEMapMgr.cs
+++ Dolany.QQAI.Plugins.CQP/Dolany.WorldLine.Standard/Ai/Game/SwordExplore/SEMapMgr.cs
@@ -14,6 +14,8 @@ namespace Dolany.WorldLine.Standard.Ai.Game.SwordExplore
 
         public SESceneModel this[string SceneID] => Map.FirstOrDefault(p => p.ID == SceneID);
 
+        public SESceneModel DefaultScene => this[Areas.First().DefaultScene];
+
         private SEMapMgr()
         {
             Map = CommonUtil.ReadJsonData<SESceneModel[]>("SE/SEMapData");
@@ -29,62 +31,5 @@ namespace Dolany.WorldLine.Standard.Ai.Game.SwordExplore
         {
             return Areas.FirstOrDefault(p => p.Name == AreaName);
         }
-    }
-
-    public class SEAreaModel : INamedJsonModel
-    {
-        public string Name { get; set; }
-
-        public string Description { get; set; }
-
-        public string[] Scenes { get; set; }
-
-        public string DefaultScene { get; set; }
-
-        public string[] NeighbourAreas { get; set; }
-    }
-
-    public class SESceneModel
-    {
-        public string ID { get; set; }
-
-        public string Name { get; set; }
-
-        public string Description { get; set; }
-
-        /// <summary>
-        /// 场景类型：Town/Boss/Wild
-        /// </summary>
-        public string Type { get; set; }
-
-        public string[] AccessableScenes { get; set; }
-
-        #region Wild
-
-        public string[] Monsters { get; set; }
-
-        public int MonsterRate { get; set; }
-
-        /// <summary>
-        /// 空闲时的文字提示
-        /// </summary>
-        public string IdleWords { get; set; }
-
-        #endregion
-
-        #region Boss
-
-        public string BossName { get; set; }
-
-        /// <summary>
-        /// Boss重生时间间隔（小时）
-        /// </summary>
-        public int BossRebornInterval { get; set; }
-
-        public string HiddenBoss { get; set; }
-
-        public int HiddenBossRate { get; set; }
-
-        #endregion
     }
 }
---
+++ Dolany.QQAI.Plugins.CQP/Dolany.WorldLine.Standard/Ai/Game/SwordExplore/SERecord.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using Dolany.Database;
+
+namespace Dolany.WorldLine.Standard.Ai.Game.SwordExplore
+{
+    public class SERecord : DbBaseEntity
+    {
+        public long QQNum { get; set; }
+
+        public int MaxHP { get; set; }
+
+        public int CurHP { get; set; }
+
+        public int MaxEndurance { get; set; }
+
+        public int CurEndurance { get; set; }
+
+        public string CurScene { get; set; }
+
+        public List<string> ClearAreas { get; set; } = new List<string>();
+
+        public Dictionary<string, int> Materials { get; set; } = new Dictionary<string, int>();
+
+        public SuperWeapon SuperWeapon { get; set; }
+
+        public static SERecord Get(long QQNum)
+        {
+            var rec = MongoService<SERecord>.GetOnly(p => p.QQNum == QQNum);
+            if (rec != null)
+            {
+                return rec;
+            }
+
+            rec = new SERecord(){QQNum = QQNum, CurScene = SEMapMgr.Instance.DefaultScene.ID};
+            MongoService<SERecord>.Insert(rec);
+            return rec;
+        }
+
+        public void Update()
+        {
+            MongoService<SERecord>.Update(this);
+        }
+    }
+
+    public class SuperWeapon
+    {
+        public string Name { get; set; }
+
+        public string Kind { get; set; }
+
+        public int Level { get; set; }
+
+        public int Exp { get; set; }
+
+        public List<MagicJewelry> EmbedJewelrys { get; set; } = new List<MagicJewelry>();
+    }
+
+    public class MagicJewelry
+    {
+        public string Name { get; set; }
+
+        public string Kind { get; set; }
+
+        public Dictionary<string, int> Affects { get; set; } = new Dictionary<string, int>();
+    }
+}
