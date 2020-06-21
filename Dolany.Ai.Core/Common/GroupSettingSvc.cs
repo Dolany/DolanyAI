ea0ea0f3350e466dd5ad51da19d5d85eaa9b23e0
Dolany <dolany@sina.cn>
2020-06-21 23:47:33 +0800

    Group直接从数据库拿

--- Dolany.Ai.Core/Common/GroupSettingSvc.cs
+++ Dolany.Ai.Core/Common/GroupSettingSvc.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using Dolany.Ai.Common;
 using Dolany.Ai.Core.Cache;
 using Dolany.Database;
@@ -13,7 +12,7 @@ namespace Dolany.Ai.Core.Common
         public static IEnumerable<GroupSettings> AllGroups =>
             RapidCacher.GetCache("AllGroups", TimeSpan.FromMinutes(10), () => MongoService<GroupSettings>.Get(p => !p.ForcedShutDown));
 
-        public GroupSettings this[long GroupNum] => AllGroups.FirstOrDefault(g => g.GroupNum == GroupNum);
+        public GroupSettings this[long GroupNum] => MongoService<GroupSettings>.GetOnly(g => g.GroupNum == GroupNum);
 
         public static void Delete(long GroupNum)
         {
